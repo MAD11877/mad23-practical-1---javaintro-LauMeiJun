76e419b9e5be6e02d69bf7afb97bbf43a55883ec
LauMeiJun <122443027+LauMeiJun@users.noreply.github.com>
2023-04-17 13:23:34 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -17,7 +17,20 @@ public class Question4
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
      
+    System.out.println("Please enter a number");
     Scanner in = new Scanner(System.in);
+    int num = in.nextInt();
+    while(num != 0){
+      StringBuilder builder = new StringBuilder(num);
+      for (int i = 0; i < num; i++) {
+        builder.append("*");
+      }
+      String newString = builder.toString();
+      System.out.println(newString);
+      num--;
+    }
+    in.close();
+  }
     
   }
 }
