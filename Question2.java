ea4da1b2b64575fee9a1ea524911b757df7f3d08
LauMeiJun <122443027+LauMeiJun@users.noreply.github.com>
2023-04-17 11:22:58 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -4,10 +4,11 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    /**
+    /*
      * Prompts user for height and weight, calculate and print out the BMI of the user
      * 
-     * Hint: in.nextDouble() to read double*/
+     * Hint: in.nextDouble() to read double
+     */
     System.out.println("Please enter your height");
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
